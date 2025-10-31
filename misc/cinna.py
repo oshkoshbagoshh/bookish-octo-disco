57bd9b269327f50836cc6dec85005b5973d4134a
AJ Javadi <amirjavadi25@gmail.com>
2025-10-31 10:47:22 -0500

    initial main

---
+++ misc/cinna.py
@@ -0,0 +1,19 @@
+# -------------------------
+# Isaac Cinna Test Profile
+# ------------------------
+
+# config / env variables
+CONFIG = {
+    "name": "Isaac Cinna",
+    "age": 30,
+    "location": "New York",
+}
+
+def do_something():
+    """This function does something."""
+    pass
+
+def do_something_else():
+    """This function does something else."""
+    pass
+
